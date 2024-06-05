b5157be8ab728aaf7efd04beefbf95ca282fc75d
piaomu <driftwood.wahl@gmail.com>
2024-06-05 09:54:07 -0500

    Update trainer and setupitem models

---
+++ Models/SetupItem.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PSDK.Trainer.Generator.Models
+{
+    public abstract class SetupItem
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        [JsonPropertyName("value")]
+        public dynamic? Value { get; set; }
+    }
+
+
+    public class EvsIvsValue : SetupItem
+    {
+        public new EffortOrIndividualValues Value { get; set; } = new();
+    }
+
+    public class LoyaltyValue : SetupItem
+    {
+        public new int Value { get; set; } = 70;
+    }
+
+    public class MovesValue : SetupItem
+    {
+        public new List<string> Value { get; set; } = [];
+    }
+
+    public class OriginalTrainerName : SetupItem
+    {
+        public new string Value { get; set; } = "Red";
+    }
+    public class OriginalTrainerId : SetupItem
+    {
+        public new int Value { get; set; } = 000001;
+    }
+
+
+    public class EffortOrIndividualValues
+    {
+        [JsonPropertyName("hp")]
+        public int Hp { get; set; }
+
+        [JsonPropertyName("atk")]
+        public int Atk { get; set; }
+
+        [JsonPropertyName("dfe")]
+        public int Def { get; set; }
+
+        [JsonPropertyName("spd")]
+        public int Spd { get; set; }
+
+        [JsonPropertyName("ats")]
+        public int SpAtk { get; set; }
+
+        [JsonPropertyName("dfs")]
+        public int SpDef { get; set; }
+    }
+}
--- Models/Trainer.cs
+++ Models/Trainer.cs
@@ -1,62 +1,154 @@
-﻿namespace PSDK.Trainer.Generator.Models
+﻿using System.Text.Json.Serialization;
+
+namespace PSDK.Trainer.Generator.Models
 {
     public class Trainer
     {
+        [JsonPropertyName("klass")]
         public string Klass { get; set; } = "TrainerBattleSetup";
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("dbSymbol")]
         public string DbSymbol { get; set; }
-        public int VsType { get; set; }
-        public bool IsCouple { get; set; }
-        public int BaseMoney { get; set; }
-        public List<int> BagEntries { get; set; } = [];
-        public int BattleId { get; set; }
-        public int Ai { get; set; }
-        public List<Pokemon?> Party { get; set; } = [];
+        [JsonPropertyName("trainerName")]
+        public string? TrainerName { get; set; }
+        public string? TrainerClass { get; set; }
+        [JsonPropertyName("vsType")]
+        public int VsType { get; set; } = 1;
+        [JsonPropertyName("isCouple")]
+        public bool IsCouple { get; set; } = false;
+        [JsonPropertyName("baseMoney")]
+        public int BaseMoney { get; set; } = 20;
+        [JsonPropertyName("bagEntries")]
+        public List<BagEntry> BagEntries { get; set; } = [];
+        [JsonPropertyName("battleId")]
+        public int BattleId { get; set; } = 0;
+        [JsonPropertyName("ai")]
+        public int Ai { get; set; } = 1;
+        [JsonPropertyName("party")]
+        public List<PartyPokemon?> Party { get; set; } = [];
+        [JsonPropertyName("resources")]
         public Resources Resources { get; set; } = new();
     }
 
-    public class Pokemon
+    public class BagEntry
     {
-        public string Specie { get; set; }
-        public int Form { get; set; }
+        public string DbSymbol { get; set; }
+        public int Amount { get; set; }
+    }
+    public class PartyPokemon
+    {
+        [JsonPropertyName("specie")]
+        public string Species { get; set; }
+        [JsonPropertyName("form")]
+        public int Form { get; set; } = 0;
+        [JsonPropertyName("shinySetup")]
         public ShinySetup ShinySetup { get; set; } = new();
+        [JsonPropertyName("levelSetup")]
         public LevelSetup LevelSetup { get; set; } = new();
-        public int RandomEncounterChance { get; set; }
-        public List<ExpandPokemonSetup> ExpandPokemonSetup { get; set; } = new List<ExpandPokemonSetup>();
+        [JsonPropertyName("randomEncounterChance")]
+        public int RandomEncounterChance { get; set; } = 1;
+        [JsonPropertyName("expandPokemonSetup")]
+        public List<ExpandPokemonSetup> ExpandPokemonSetup { get; set; } = [];
     }
 
     public class ShinySetup
     {
-        public string Kind { get; set; }
-        public int Rate { get; set; }
+        public string Kind { get; set; } = "automatic";
+        public int Rate { get; set; } = -1;
     }
 
     public class LevelSetup
     {
-        public string Kind { get; set; }
-        public int Level { get; set; }
+        public string Kind { get; set; } = "fixed";
+        public int Level { get; set; } = 5;
     }
 
     public class ExpandPokemonSetup
     {
-        public string Type { get; set; }
-        public object Value { get; set; } // Using 'object' to accommodate different value types
+        public ExpandPokemonSetup()
+        {
+            SetupItems =
+            [
+                new EvsIvsValue() //evs
+                {
+                    Type = "evs",
+                    Value = new()
+                    {
+                        Hp = 0,
+                        Atk = 0,
+                        Def = 0,
+                        SpAtk = 0,
+                        SpDef = 0,
+                        Spd = 0
+                    }
+                },
+                new EvsIvsValue() //ivs
+                {
+                    Type = "ivs",
+                    Value = new()
+                    {
+                        Hp = 0,
+                        Atk = 0,
+                        Def = 0,
+                        SpAtk = 0,
+                        SpDef = 0,
+                        Spd = 0
+                    }
+                },
+
+                new LoyaltyValue()
+                {
+                    Type = "loyalty",
+                    Value = 70
+                },
+                new MovesValue()
+                {
+                    Type = "moves",
+                    Value = new List<string>
+                    {
+                        "__undef__",
+                        "__undef__",
+                        "__undef__",
+                        "__undef__"
+                    }
+                },
+                new OriginalTrainerName()
+                {
+                    Type = "originalTrainerName"
+                },
+                new OriginalTrainerId()
+                {
+                    Type = "originalTrainerId"
+                }
+            ];
+        }
+        public List<SetupItem> SetupItems { get; set; } = [];
     }
 
     public class Resources
     {
+        [JsonPropertyName("sprite")]
         public string? Sprite { get; set; }
+        [JsonPropertyName("artworkFull")]
         public string? ArtworkFull { get; set; }
+        [JsonPropertyName("artworkSmall")]
         public string? ArtworkSmall { get; set; }
+        [JsonPropertyName("character")]
         public string? Character { get; set; }
+        [JsonPropertyName("musics")]
         public Musics Musics { get; set; } = new();
     }
 
     public class Musics
     {
+        [JsonPropertyName("encounter")]
         public string? Encounter { get; set; }
+        [JsonPropertyName("victory")]
         public string? Victory { get; set; }
+        [JsonPropertyName("defeat")]
         public string? Defeat { get; set; }
+        [JsonPropertyName("bgm")]
         public string? Bgm { get; set; }
     }
 }
