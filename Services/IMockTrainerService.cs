02f2f94293e0dd7091d57a0532c9d8eef7e061f7
piaomu <driftwood.wahl@gmail.com>
2024-06-06 15:45:23 -0500

    Add mock trainer service

---
+++ Services/IMockTrainerService.cs
@@ -0,0 +1,10 @@
+﻿using PSDK.Trainer.Generator.Models;
+
+namespace PSDK.Trainer.Generator.Services
+{
+    public interface IMockTrainerService
+    {
+        public Task<List<PokeTrainer>> GetPokeTrainersAsync();
+        public Task<PokeTrainer> GetPokeTrainerByIdAsync(string id);
+    }
+}
---
+++ Services/MockTrainerService.cs
@@ -0,0 +1,17 @@
+﻿using PSDK.Trainer.Generator.Models;
+
+namespace PSDK.Trainer.Generator.Services
+{
+    public class MockTrainerService : IMockTrainerService
+    {
+        public Task<PokeTrainer> GetPokeTrainerByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<PokeTrainer>> GetPokeTrainersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
