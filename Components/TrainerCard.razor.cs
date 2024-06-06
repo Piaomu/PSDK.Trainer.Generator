c7bdcb1b399a438f46ef9c42d01c894c31d8dbf7
piaomu <driftwood.wahl@gmail.com>
2024-06-06 15:29:34 -0500

    Add TrainerCard component

---
+++ Components/TrainerCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using PSDK.Trainer.Generator.Models;
+namespace PSDK.Trainer.Generator.Components
+{
+    public partial class TrainerCard
+    {
+        [Parameter]
+        public PokeTrainer? Trainer { get; set; } = default;
+    }
+}
